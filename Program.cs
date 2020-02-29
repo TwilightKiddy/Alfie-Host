a606741cd3e08057b83a88154e53f003520cbf2c
Twilight Kiddy <kiddy97rus@gmail.com>
2020-02-29 05:06:44 +0300

    This version worked normally for 9 days. Changed date format in logs.

--- Program.cs
+++ Program.cs
@@ -102,7 +102,7 @@ namespace Alfie_Host
             if(msg.Exception != null)
                 if(msg.Exception.Message == "WebSocket connection was closed")
                     return Task.CompletedTask;
-            Console.WriteLine(msg.ToString(fullException: false));
+            Console.WriteLine(DateTime.Now.ToString("[dd.MM.yyyy hh:mm:ss] ") + msg.ToString(fullException: false, prependTimestamp: false));
             return Task.CompletedTask;
         }
 
